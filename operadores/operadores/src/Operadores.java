64b6f94a489d4ce3a49e27277b877475b77d7f14
amanjonson22 <amandacsr10@gmail.com>
2023-09-04 21:03:39 -0300

    operadores

---
+++ operadores/operadores/src/Operadores.java
@@ -0,0 +1,29 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
+    }
+}
