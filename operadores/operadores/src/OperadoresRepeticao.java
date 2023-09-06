0c4df0300441c48735c1481cc9bf39cf6a196761
amanjonson22 <amandacsr10@gmail.com>
2023-09-05 22:27:42 -0300

    operadores

---
+++ operadores/operadores/src/OperadoresRepeticao.java
@@ -0,0 +1,35 @@
+public class OperadoresRepeticao {
+    public static void main(String[] args){
+        int numero = 5;
+
+        //x repeticao
+        numero++;
+        //numero = numero + 1
+
+        System.out.println(numero++);
+
+        System.out.println(numero);
+
+        System.out.println(++numero);
+
+        System.out.println(numero--);
+
+        System.out.println(numero);
+
+        System.out.println(--numero);
+
+        boolean variavel = true;
+
+        System.out.println(variavel);
+
+        System.out.println(!variavel);
+
+        System.out.println(variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+        System.out.println(!variavel);
+    }
+}
